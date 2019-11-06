f34f0357c8029bdd78fe84669b2c47594166b1a4
Megan <megangeisreiter@gmail.com>
2019-11-06 15:32:09 -0500

    updating

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 import pandas as pd
 
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+import plotly as py
+from plotly.graph_objs import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
 ########### Define a few variables ######
 
-tabtitle = 'Dash Template'
-sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/dash-template'
+tabtitle = 'NYC Green Taxis'
+sourceurl = 'https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
+githublink = 'https://github.com/mgeisreiter/dash-template'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -17,11 +25,94 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
+taxi = pd.read_csv('green_tripdata_2019-01.csv')
+lookup = pd.read_csv('taxi+_zone_lookup.csv')
+fp = 'geo_export_5741a122-d32a-497b-9ea7-73a4e2ea6bcb.shx'
+map_df = gpd.read_file(fp)
+df2 = pd.merge(taxi, lookup, left_on= 'PULocationID', right_on='LocationID', how = 'inner')
+merged = pd.merge(df2, map_df, left_on= 'Borough', right_on='boro_name', how = 'inner')
+merged = merged.drop(['boro_code', 'service_zone', 'Zone', 'congestion_surcharge','RatecodeID', 'store_and_fwd_flag', 'LocationID' , 'boro_name'], axis=1)
+merged = merged.rename(columns={"shape_area": "PUshape_area", "shape_leng": "PUshape_leng", 'geometry':'PUgeometry', 'boro_code':'PUboro_code', 'Borough':'PUBorough'})
+merged2 = pd.merge(merged, lookup, left_on= 'DOLocationID', right_on='LocationID', how = 'inner')
+merged3 = pd.merge(merged2, map_df, left_on= 'Borough', right_on='boro_name', how = 'inner')
+merged3 = merged3.drop(['boro_code', 'service_zone', 'Zone', 'LocationID','boro_name' ], axis=1)
+merged3 = merged3.rename(columns={"shape_area": "DOshape_area", "shape_leng": "DOshape_leng", 'geometry':'DOgeometry', 'Borough':'DOBorough', 'total_amount': 'Avg Fare', 'trip_distance': 'Avg Distance', 'passenger_count':'Avg # Passengers'})
+merged3['pu_datetime'] = pd.to_datetime(merged3['lpep_pickup_datetime'])
+merged3['do_datetime'] = pd.to_datetime(merged3['lpep_dropoff_datetime'])
+merged3=merged3[merged3['pu_datetime'].dt.year == 2019]
+merged3['pu_date']=merged3['pu_datetime'].dt.date
+
+pu_boroughs = ['Manhattan', 'Queens','Brooklyn', 'Bronx', 'Staten Island']
+do_boroughs = ['Manhattan', 'Queens','Brooklyn', 'Bronx', 'Staten Island']
+attributes  = ['Avg Fare', 'Avg Distance', 'Avg # Passengers']
+
+def update_fig(pu_selected, do_selected, atr):
+    new_df=merged3[(merged3['PUBorough']==pu_selected)&(merged3['DOBorough']==do_selected)]
+    #new_df=merged3[merged3['DOBorough']=='Queens']
+    mean = new_df.groupby('DOBorough')['Avg Fare'].mean()
+
+    mydata4 = go.Scatter(x = new_df['pu_date'],
+                    y=new_df.groupby('pu_date')['Avg Distance'].mean(),
+                    marker = dict(color='#122A7F'),
+                    name = 'Distance')
+    mylayout = go.Layout(
+                    title = 'Test',
+                    xaxis = dict(title='Borough'),
+                    yaxis = dict(title= 'Total Amount'))
+    fig = go.Figure(data=[mydata4], layout = mylayout)
+    return fig
+
+
+def stats(pu_selected, do_selected):
+     nl = 'x'
+     new_df=merged3[(merged3['PUBorough']==pu_selected)&(merged3['DOBorough']==do_selected)]
+     avg_fare = dict(new_df.groupby('PUBorough')['Avg Fare'].mean())
+     avg_dist = dict(new_df.groupby('PUBorough')['Avg Distance'].mean())
+     avg_pass = dict(new_df.groupby('PUBorough')['Avg # Passengers'].mean())
+     text = (f'The average taxi trip from {pu_selected} to {do_selected} has the following values: ')
+     text2 = (f'Avg Fare: {round(avg_fare[pu_selected],1)}')
+     text3 = (f'Avg Distance: {round(avg_dist[pu_selected],1)}')
+     text4 = (f'Avg # Passengers: {round(avg_pass[pu_selected],1)}')
+
+     x = ['Stat', 'Value']
+     y = [['Avg Fare', 'Avg Distance', 'Avg # Passengers'], [round(avg_fare[pu_selected],1), round(avg_dist[pu_selected],1), round(avg_pass[pu_selected],1)]]
+     table = go.Figure(data=go.Table(header = dict(values = x), cells=dict(values = y)))
+
+     return table
+
+     #return text + '\n' + text2 + text3 + text4
+
 ########### Layout
 
 app.layout = html.Div(children=[
-    html.H1('This is the title'),
-    html.Div(children=['Stuff will go here']),
+    html.H1('Green Taxi Trips in NYC'),
+    html.Br(),
+    html.Div([
+        html.Div([
+            dcc.Dropdown(id='pu-location',
+                    options=[{'label': i, 'value': i} for i in pu_boroughs],
+                    value='Brooklyn',
+                    style={'width': '5 00px'}),
+        ], className= 'six columns') ,
+        html.Div([
+            dcc.Dropdown(id='do-location',
+                    options=[{'label': i, 'value': i} for i in do_boroughs],
+                    value='Queens',
+                    style={'width': '500px'}),
+        ], className = 'six columns'),
+    ], className = 'twelve columns'),
+    html.Br(),
+    html.Br(),
+    html.Div(id='stats_values', children=''),
+    dcc.Graph(id='stats_table', figure = update_fig('Brooklyn', 'Queens', 'Avg Fare')),
+    html.Br(),
+    html.Div([
+        dcc.Dropdown(id='attribute',
+                options=[{'label': i, 'value': i} for i in attributes],
+                value='Avg Fare',
+                style={'width': '500px'})]),
+    html.Br(),
+    dcc.Graph(id='display-value', figure = stats('Brooklyn', 'Queens')),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
@@ -29,7 +120,43 @@ app.layout = html.Div(children=[
 )
 
 ############ Callbacks
+@app.callback(dash.dependencies.Output('display-value', 'figure'),
+              [dash.dependencies.Input('pu-location', 'value'),
+              dash.dependencies.Input('do-location', 'value'),
+              dash.dependencies.Input('attribute', 'value')])
+def update_fig(pu_selected, do_selected, atr):
+    new_df=merged3[(merged3['PUBorough']==pu_selected)&(merged3['DOBorough']==do_selected)]
+    new_df = new_df.sort_values(by='pu_date')
+    mydata4 = go.Scatter(x = new_df['pu_date'].unique(),
+                    y=new_df.groupby(['pu_date'])[atr].mean())
+    mylayout = go.Layout(
+                    title = f'Select an attribute above to learn more about taxi trips from {pu_selected} to {do_selected} in January 2019',
+                    xaxis = dict(title='Date'),
+                    yaxis = dict(title= atr))
+    fig = go.Figure(data=[mydata4], layout = mylayout)
+    return fig
 
+@app.callback(dash.dependencies.Output('stats_table', 'figure'),
+              [dash.dependencies.Input('pu-location', 'value'),
+              dash.dependencies.Input('do-location', 'value')])
+def stats(pu_selected, do_selected):
+     new_df=merged3[(merged3['PUBorough']==pu_selected)&(merged3['DOBorough']==do_selected)]
+     avg_fare = dict(new_df.groupby('PUBorough')['Avg Fare'].mean())
+     avg_dist = dict(new_df.groupby('PUBorough')['Avg Distance'].mean())
+     avg_pass = dict(new_df.groupby('PUBorough')['Avg # Passengers'].mean())
+
+     x = ['Attribute', 'Value']
+     y = [['Avg. Fare', 'Avg. Distance', 'Avg. # Passengers'], [round(avg_fare[pu_selected],1), round(avg_dist[pu_selected],1), round(avg_pass[pu_selected],1)]]
+     table = go.Figure(data=go.Table(header = dict(values = x), cells=dict(values = y)))
+     #table.update_layout(width=500, height=300)
+     return table
+
+@app.callback(dash.dependencies.Output('stats_values', 'children'),
+              [dash.dependencies.Input('pu-location', 'value'),
+              dash.dependencies.Input('do-location', 'value')])
+def stats(pu_selected, do_selected):
+     text = (f'The average taxi trip from {pu_selected} to {do_selected} has the following attributes: ')
+     return text
 
 
 ############ Deploy
